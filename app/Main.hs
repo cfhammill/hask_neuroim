59d0e804ed9ee07495fed2ecd62cb21cc661fc75
cfhammill <cfhammill@gmail.com>
2017-09-10 21:24:27 -0400

    Fix demo app to build properly

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,16 @@
 module Main where
 
 import Lib
+import Data.Array.Repa
+import qualified Data.Array.Repa as Repa
 
 main :: IO ()
-main = do
-  let x = fromListUnboxed (Z :. (3::Int) :. (3::Int) :. (3::Int)) [1..27] ::
-        Array U DIM3 Double;
-  let i4 =
-        fromListUnboxed
-          (Z :. (4::Int) :. (4::Int))
-          [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-  print $ (computeP $ resample x (flip affine i4) trilinear  :: IO (Array U DIM3 Double))
-  
+main =
+  do let x = fromListUnboxed (Z :. (3::Int) :. (3::Int) :. (3::Int)) [1..27] ::
+           Array U DIM3 Double
+     let xfm = fromListUnboxed
+              (Z :. (4::Int) :. (4::Int))
+              [0.9, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]    
+     let res = computeS $ resample x (flip affine xfm) trilinear :: Array U DIM3 Double
+     print res
+     
